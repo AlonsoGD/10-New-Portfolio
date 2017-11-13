fc78d9b4e4aa29d6b01118a76a601d3bbc8c9b84
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-11-13 16:24:48 -0500

    Finished mainpage layout and animatios

--- js/script.js
+++ js/script.js
@@ -1,53 +1,26 @@
 $(document).ready(function () {
     var $buttons = $('button');
-
-    //var $individualButtons = [$('#about'), $('#skills'), $('#portfolio'), $('#didbefore'), $('#contact')];
-    var $about = $('#about');
-    var $skills = $('#skills');
-    var $portfolio = $('#portfolio');
-    var $didBefore = $('#didbefore');
-    var $contact = $('#contact');
+    var $buttonsArray = [$('#aboutb'), $('#skillsb'), $('#portfoliob'), $('#didbeforeb'), $('#contactb')];
     var $titles =  $('.titles');
 
-    // function introAnimation () {
-    //     var sleep = 400;
-    //     for (var i = 0; i < $individualButtons; i++) {
-    //         setTimeout(function () {
-    //             $individualButtons[i].addClass('animated fadeInRight');
-    //             $individualButtons[i].css('opacity', 1);
-    //             sleep += 100;
-    //             console.log(sleep);
-    //         }, sleep);
-    //     }
-    // }
+    function introAnimation () {
+        
+        function animationWithTimeout (element, delay) {
+            setTimeout(function () {
+                element.css('opacity', '1');
+                element.addClass('animated fadeInRight');
+            }, delay);
+        }
 
-    // introAnimation();
-    
+        var delay = 500;  
+        for (var i = 0; i < $buttonsArray.length; i++) {
+            animationWithTimeout($buttonsArray[i], delay);
+            delay += 100;
+        };
+    }
+   
     $titles.addClass('animated fadeInUp');
-    setTimeout(function () {
-        $about.css('opacity', '1');
-        $about.addClass('animated fadeInRight');
-    }, 500);
-
-    setTimeout(function () {
-        $skills.css('opacity', '1');
-        $skills.addClass('animated fadeInRight');
-    }, 600);
-
-    setTimeout(function () {
-        $portfolio.css('opacity', '1');
-        $portfolio.addClass('animated fadeInRight');
-    }, 700);
-
-    setTimeout(function () {
-        $didBefore.css('opacity', '1');
-        $didBefore.addClass('animated fadeInRight');
-    }, 800);
-
-    setTimeout(function () {
-        $contact.css('opacity', '1');
-        $contact.addClass('animated fadeInRight');
-    }, 900);
+    introAnimation();
 
     //Click animations for the buttons;
     $buttons
