d1e16dc3521f1e80e4a5a6e48510f9c49bcbeecc
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-11-13 14:15:22 -0500

    revert to 13ed4cd8bebdb3b9cdc650bdb79560afeb933920

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     var $buttons = $('button');
 
-    // var $about = $('#about');
-    // var $skills = $('#skills');
-    // var $portfolio = $('#portfolio');
-    // var $didBefore = $('#didbefore');
-    // var $contact = $('#contact');
-    // var $titles =  $('.titles');
     //var $individualButtons = [$('#about'), $('#skills'), $('#portfolio'), $('#didbefore'), $('#contact')];
+    var $about = $('#about');
+    var $skills = $('#skills');
+    var $portfolio = $('#portfolio');
+    var $didBefore = $('#didbefore');
+    var $contact = $('#contact');
+    var $titles =  $('.titles');
 
     // function introAnimation () {
     //     var sleep = 400;
@@ -22,30 +22,30 @@ $(document).ready(function () {
     // }
 
     // introAnimation();
-    $('.titles').addClass('animated fadeInUp');
+    $titles.addClass('animated fadeInUp');
     setTimeout(function () {
-        $('#about').css('opacity', '1');
-        $('#about').addClass('animated fadeInRight');
+        $about.css('opacity', '1');
+        $about.addClass('animated fadeInRight');
     }, 500);
 
     setTimeout(function () {
-        $('#skills').css('opacity', '1');
-        $('#skills').addClass('animated fadeInRight');
+        $skills.css('opacity', '1');
+        $skills.addClass('animated fadeInRight');
     }, 600);
 
     setTimeout(function () {
-        $('#portfolio').css('opacity', '1');
-        $('#portfolio').addClass('animated fadeInRight');
+        $portfolio.css('opacity', '1');
+        $portfolio.addClass('animated fadeInRight');
     }, 700);
 
     setTimeout(function () {
-        $('#didbefore').css('opacity', '1');
-        $('#didbefore').addClass('animated fadeInRight');
+        $didBefore.css('opacity', '1');
+        $didBefore.addClass('animated fadeInRight');
     }, 800);
 
     setTimeout(function () {
-        $('#contact').css('opacity', '1');
-        $('#contact').addClass('animated fadeInRight');
+        $contact.css('opacity', '1');
+        $contact.addClass('animated fadeInRight');
     }, 900);
 
     //Click animations for the buttons;
