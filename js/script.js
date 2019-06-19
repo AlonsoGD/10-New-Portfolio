988c86c4fe82d945db553a4a6e7eb7988c2fff40
AlonsoGD <dani.alonso.garcia@gmail.com>
2019-06-18 21:10:05 -0400

    Mobile fixes

--- js/script.js
+++ js/script.js
@@ -1,158 +1,158 @@
-$(document).ready(function () {
-    var $allButtons = $('button');
-    var $allMainButtons = $('.mainbuttonskin');
-    var $allBackButtons = $('.homebutton');
-    var $allScreens = $('.screen');
-    var $pagelayout = $('.pagelayout');
-    var $buttonsObj = {
-        aboutmeb : $('#aboutb'),
-        skillsb : $('#skillsb'), 
-        portfoliob : $('#portfoliob'), 
-        previousexpb : $('#didbeforeb'), 
-        contactb : $('#contactb')
-    };
-    var $screensObj = {
-        mainpage : $('#mainpage'),
-        aboutme : $('#aboutme'), 
-        skills : $('#skills'), 
-        portfolio : $('#porfolio'), 
-        previusexp : $('#previousexp'), 
-        contact : $('#contact')
-    };
-    var $titles =  $('.titles');
+$(document).ready(function() {
+  var $body = $("body");
+  var $allButtons = $("button");
+  var $allMainButtons = $(".mainbuttonskin");
+  var $allBackButtons = $(".homebutton");
+  var $allScreens = $(".screen");
+  var $pagelayout = $(".pagelayout");
+  var $buttonsObj = {
+    aboutmeb: $("#aboutb"),
+    skillsb: $("#skillsb"),
+    portfoliob: $("#portfoliob"),
+    previousexpb: $("#didbeforeb"),
+    contactb: $("#contactb")
+  };
+  var $screensObj = {
+    mainpage: $("#mainpage"),
+    aboutme: $("#aboutme"),
+    skills: $("#skills"),
+    portfolio: $("#porfolio"),
+    previusexp: $("#previousexp"),
+    contact: $("#contact")
+  };
+  var $titles = $(".titles");
 
-    function introAnimation () {
-        
-        function animationWithTimeout (element, delay) {
-            setTimeout(function () {
-                element.css('opacity', '1');
-                element.addClass('animated fadeInRight');
-            }, delay);
-        }
-
-        hideAllPages();
-        showElement($screensObj.mainpage);
-        $titles.addClass('animated fadeInUp');
-        
-        var delay = 500;  
-        for (var key in $buttonsObj) {
-            if ($buttonsObj.hasOwnProperty(key)) {
-                animationWithTimeout($buttonsObj[key], delay);
-                delay += 100;
-            }
-        }
+  function introAnimation() {
+    function animationWithTimeout(element, delay) {
+      setTimeout(function() {
+        element.css("opacity", "1");
+        element.addClass("animated fadeInRight");
+      }, delay);
     }
 
-    function hideAllPages() {
-        hideElement($allScreens);
-    }
-    
-    function hideElement (element) {
-        element.hide();
-        element.css('opacity', '0');
-    }
-    
-    function showElement (element) {
-        element.show();
-        element.css('opacity', '1');
-    }
-    
-    function toggleBackground () {
-        $pagelayout.toggleClass('darken');
-    }
-    
-    function hideMainPage () {
-        hideElement($screensObj.mainpage);
-        toggleBackground();
-    }
-    
-    function goBack () {
-        hideAllPages();
-        showElement($screensObj.mainpage);
-        toggleBackground();
-    }
+    hideAllPages();
+    showElement($screensObj.mainpage);
+    $titles.addClass("animated fadeInUp");
 
-    // Thumb nail code//
-	$('.zoom-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		closeOnContentClick: false,
-		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom mfp-img-mobile',
-		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot;';
-			}
-		},
-		gallery: {
-			enabled: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
-				return element.find('img');
-			}
-		}
+    var delay = 500;
+    for (var key in $buttonsObj) {
+      if ($buttonsObj.hasOwnProperty(key)) {
+        animationWithTimeout($buttonsObj[key], delay);
+        delay += 100;
+      }
+    }
+  }
 
-	});
+  function hideAllPages() {
+    hideElement($allScreens);
+  }
 
-    // MAIN PAGE //
+  function hideElement(element) {
+    element.hide();
+    element.css("opacity", "0");
+  }
 
-    introAnimation();
-        
-    //Click listeners for the animations and page changes
-    //Animations when main menu buttons clicked
-    $allButtons
-        .mousedown(function() {
-            $(this).addClass('clicked');
-        })
-        .mouseup(function() {
-            $(this).removeClass('clicked');
-        })
-    
-    //Clicking about me button
-    $buttonsObj.aboutmeb.click(function (){
-        hideMainPage();
-        showElement($screensObj.aboutme);
-        history.pushState(null, null, "aboutme")
+  function showElement(element) {
+    element.show();
+    element.css("opacity", "1");
+  }
+
+  function toggleBackground() {
+    //$body.toggleClass("darken");
+    $pagelayout.toggleClass("darken");
+  }
+
+  function hideMainPage() {
+    hideElement($screensObj.mainpage);
+    toggleBackground();
+  }
+
+  function goBack() {
+    hideAllPages();
+    showElement($screensObj.mainpage);
+    toggleBackground();
+  }
+
+  // Thumb nail code//
+  $(".zoom-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: "mfp-with-zoom mfp-img-mobile",
+    image: {
+      verticalFit: true,
+      titleSrc: function(item) {
+        return item.el.attr("title") + " &middot;";
+      }
+    },
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+      opener: function(element) {
+        return element.find("img");
+      }
+    }
+  });
+
+  // MAIN PAGE //
+
+  introAnimation();
+
+  //Click listeners for the animations and page changes
+  //Animations when main menu buttons clicked
+  $allButtons
+    .mousedown(function() {
+      $(this).addClass("clicked");
     })
-
-    //Clicking skills button
-    $buttonsObj.skillsb.click(function (){
-        hideMainPage();
-        showElement($screensObj.skills);
-        history.pushState(null, null, "skills");
-    })
-    //Clicking porfolio button
-    $buttonsObj.portfoliob.click(function (){
-        hideMainPage();
-        showElement($screensObj.portfolio);
-        history.pushState(null, null, "portfolio");
-    })
-    //Clicking previous experience button
-    $buttonsObj.previousexpb.click(function (){
-        hideMainPage();
-        showElement($screensObj.previusexp);
-        history.pushState(null, null, "previousexp");
-    })
-    //Clicking contact button
-    $buttonsObj.contactb.click(function (){
-        hideMainPage();
-        showElement($screensObj.contact);
-        history.pushState(null, null, "contact");
-    })
-    //Clicking any of the BACK Buttons;
-    $allBackButtons.click(function () {
-        $titles.removeClass('animated fadeInRight');
-        $allMainButtons.removeClass('animated fadeInRight');
-        history.pushState(null, null, "index");
-        goBack();
+    .mouseup(function() {
+      $(this).removeClass("clicked");
     });
-    window.addEventListener('popstate', function(e) {
-        $titles.removeClass('animated fadeInRight');
-        $allMainButtons.removeClass('animated fadeInRight');
-        goBack()
-    });
-})
+
+  //Clicking about me button
+  $buttonsObj.aboutmeb.click(function() {
+    hideMainPage();
+    showElement($screensObj.aboutme);
+    history.pushState(null, null, "aboutme");
+  });
+
+  //Clicking skills button
+  $buttonsObj.skillsb.click(function() {
+    hideMainPage();
+    showElement($screensObj.skills);
+    history.pushState(null, null, "skills");
+  });
+  //Clicking porfolio button
+  $buttonsObj.portfoliob.click(function() {
+    hideMainPage();
+    showElement($screensObj.portfolio);
+    history.pushState(null, null, "portfolio");
+  });
+  //Clicking previous experience button
+  $buttonsObj.previousexpb.click(function() {
+    hideMainPage();
+    showElement($screensObj.previusexp);
+    history.pushState(null, null, "previousexp");
+  });
+  //Clicking contact button
+  $buttonsObj.contactb.click(function() {
+    hideMainPage();
+    showElement($screensObj.contact);
+    history.pushState(null, null, "contact");
+  });
+  //Clicking any of the BACK Buttons;
+  $allBackButtons.click(function() {
+    $titles.removeClass("animated fadeInRight");
+    $allMainButtons.removeClass("animated fadeInRight");
+    history.pushState(null, null, "index");
+    goBack();
+  });
+  window.addEventListener("popstate", function(e) {
+    $titles.removeClass("animated fadeInRight");
+    $allMainButtons.removeClass("animated fadeInRight");
+    goBack();
+  });
+});
